18f4cce6fdbd4a081557290ef6d09741d05289a8
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2018-02-22 19:03:52 +0100

    Read from kypd (WIP)
    We get input, but there is interference...

--- mipslabwork.c
+++ mipslabwork.c
@@ -24,58 +24,106 @@ int prime = 1234567;
 /* Interrupt Service Routine */
 void user_isr( void )
 {
-  if (IFS(0) & (1 << 8)) {
-    IFSCLR(0) = 1 << 8;
-    timeoutcount++;
-
-    if (timeoutcount == 10) {
-      timeoutcount = 0;
-
-      time2string(textstring, mytime);
-      display_string(3, textstring);
-      display_update();
-      tick(&mytime);
-    }
-  }
-
-  if (IFS(0) & (1 << 7)) {
-    IFSCLR(0) = 1 << 7;
-    PORTE++;
-  }
 }
 
 /* Lab-specific initialization goes here */
 void labinit( void )
 {
-  volatile int *trise = (int *) 0xbf886100;
+  // KYPD functions
+  // C4 C3 C2 C1 GD VC
+  // R4 R3 R2 R1 GD VC
 
-  *trise &= ~0xff;
+  // KYPD pins
+  // 01 02 03 04 05 06
+  // 07 08 09 10 11 12
 
-  TRISDSET = 0xfe0;
+  // chipKit pins
+  // 05 04 03 02 01 00
+  // 31 30 29 28 27 26
 
-  PR2 = 31250;	// 0.1 s
-  TMR2 = 0;
+  // Registers
+  // D1 F1 D0 D8 F3 F2
+  // E5 E4 E3 E2 E1 E0
 
-  IECSET(0) = 1 << 8;
-  IFSCLR(0) = 1 << 8;
-  IPCSET(2) = 0x1f;	// prio=7, sub=3
+  // COLx
+  TRISDCLR = 1 << 1;
+  TRISFCLR = 1 << 1;
+  TRISDCLR = 1 << 0;
+  TRISDCLR = 1 << 8;
 
-  T2CON = (1 << 15)	// ON
-	| (7 << 4)	// 256 prescale
-	;
+  PORTDSET = 1 << 1;
+  PORTFSET = 1 << 1;
+  PORTDSET = 1 << 0;
+  PORTDSET = 1 << 8;
 
-  IECSET(0) = 1 << 7;
-  IPCSET(1) = 0x1f << 24;
+  // ROWx
+  TRISESET = 1 << 5;
+  TRISESET = 1 << 4;
+  TRISESET = 1 << 3;
+  TRISESET = 1 << 2;
 
-  PORTE = 0;
+  // VCC
+  TRISFCLR = 1 << 2;
+  TRISECLR = 1 << 0;
 
-  enable_interrupt();
+  PORTFSET = 1 << 2;
+  PORTESET = 1 << 0;
+
+  // GND
+  TRISFCLR = 1 << 3;
+  TRISECLR = 1 << 1;
+
+  PORTFCLR = 1 << 3;
+  PORTECLR = 1 << 1;
+}
+
+void keypad_read(unsigned char keys[16])
+{
+  PORTDCLR = 1 << 1;
+  keys[3 + 3 * 4] = !(PORTE & (1 << 5));
+  keys[2 + 3 * 4] = !(PORTE & (1 << 4));
+  keys[1 + 3 * 4] = !(PORTE & (1 << 3));
+  keys[0 + 3 * 4] = !(PORTE & (1 << 2));
+  PORTDSET = 1 << 1;
+
+  PORTFCLR = 1 << 1;
+  keys[3 + 2 * 4] = !(PORTE & (1 << 5));
+  keys[2 + 2 * 4] = !(PORTE & (1 << 4));
+  keys[1 + 2 * 4] = !(PORTE & (1 << 3));
+  keys[0 + 2 * 4] = !(PORTE & (1 << 2));
+  PORTFSET = 1 << 1;
+
+  PORTDCLR = 1 << 0;
+  keys[3 + 1 * 4] = !(PORTE & (1 << 5));
+  keys[2 + 1 * 4] = !(PORTE & (1 << 4));
+  keys[1 + 1 * 4] = !(PORTE & (1 << 3));
+  keys[0 + 1 * 4] = !(PORTE & (1 << 2));
+  PORTDSET = 1 << 0;
+
+  PORTDCLR = 1 << 8;
+  keys[3 + 0 * 4] = !(PORTE & (1 << 5));
+  keys[2 + 0 * 4] = !(PORTE & (1 << 4));
+  keys[1 + 0 * 4] = !(PORTE & (1 << 3));
+  keys[0 + 0 * 4] = !(PORTE & (1 << 2));
+  PORTDSET = 1 << 8;
 }
 
 /* This function is called repetitively from the main program */
 void labwork( void )
 {
-  prime = nextprime(prime);
-  display_string(0, itoaconv(prime));
+  unsigned char keys[16];
+  char str[17];
+  int i;
+
+  keypad_read(keys);
+
+  for (i = 0; i < 16; i++)
+    str[i] = keys[i] ? 'x' : 'o';
+
+  str[16] = '\0';
+
+  display_string(0, str);
   display_update();
+
+  delay(100);
 }
